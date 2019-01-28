dce7ae92a0707eacfa2ff325e3a5f2ea53d5af2d
Aaron Longwell <a@adl.io>
2019-01-27 17:36:02 -0800

    Adds array comparison for artists.

--- compare.go
+++ compare.go
@@ -1,14 +1,43 @@
 package matchdata
 
-import "github.com/texttheater/golang-levenshtein/levenshtein"
+import (
+	"github.com/gonum/stat"
+	"github.com/texttheater/golang-levenshtein/levenshtein"
+)
 
-// CompareArtistNames calculates the similarity score
+// CompareArtistNames calculates the similarity score (100
+// represents a perfect match) between arrays of artist names. Each
+// artist in the first slice is compared with each artist in the
+// second. The best scores for each artist are kept, and the average
+// match of all artists is returned.
+func CompareArtistNames(a, b []string) (score, scrubbedScore float64) {
+
+	var bestScore, bestScrubbedScore, thisScore, thisScrubbedScore float64
+	scores := make([]float64, len(a))
+	scrubbedScores := make([]float64, len(a))
+
+	for i, left := range a {
+		bestScore = 0.0
+		for _, right := range b {
+			thisScore, thisScrubbedScore = CompareArtistName(left, right)
+			if thisScrubbedScore > bestScrubbedScore {
+				bestScrubbedScore = thisScrubbedScore
+				bestScore = thisScore
+			}
+		}
+		scores[i], scrubbedScores[i] = bestScore, bestScrubbedScore
+	}
+
+	return stat.Mean(scores, nil), stat.Mean(scrubbedScores, nil)
+}
+
+// CompareArtistName calculates the similarity score
 // (100 represents a perfect match) between the two submitted
 // artist names. Two scores are returned. The first is the direct
 // comparison score. The second is the score after scrubbing
 // noise words.
 //
-func CompareArtistNames(a, b string) (score, scrubbedScore float64) {
+func CompareArtistName(a, b string) (score, scrubbedScore float64) {
 	scrubbedA, scrubbedB := ScrubArtistName(a), ScrubArtistName(b)
 	score = 1.0 - levenshtein.RatioForStrings([]rune(a), []rune(b), levenshtein.DefaultOptions)
 	scrubbedScore = 1.0 - levenshtein.RatioForStrings([]rune(scrubbedA), []rune(scrubbedB), levenshtein.DefaultOptions)
