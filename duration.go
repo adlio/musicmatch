cf3a01d182199315d279c365a6abf504474899e9
Aaron Longwell <a@adl.io>
2019-05-16 13:37:44 -0700

    Adds DurationFromString and tests.

---
+++ duration.go
@@ -0,0 +1,22 @@
+package musicmeta
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+// DurationFromString converts a song length in string
+// format (e.g. 00:00) and returns a time.Duration.
+//
+func DurationFromString(durStr string) time.Duration {
+	var d time.Duration
+	increments := []time.Duration{time.Second, time.Minute, time.Hour}
+	stringParts := strings.Split(durStr, ":")
+	for i := len(stringParts) - 1; i >= 0; i-- {
+		iPart, _ := strconv.Atoi(stringParts[i])
+		j := len(stringParts) - 1 - i
+		d += time.Duration(iPart) * increments[j]
+	}
+	return d
+}
---
+++ duration_test.go
@@ -0,0 +1,38 @@
+package musicmeta
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDurationFromEmptyString(t *testing.T) {
+	d := DurationFromString("")
+	assert.EqualValues(t, 0, d)
+}
+
+func TestDurationFromStringSecondsOnly(t *testing.T) {
+	d := DurationFromString("01")
+	assert.Equal(t, 1.0, d.Seconds())
+	d = DurationFromString("99")
+	assert.Equal(t, 99.0, d.Seconds())
+	d = DurationFromString("10")
+	assert.Equal(t, 10.0, d.Seconds())
+}
+
+func TestDurationFromStringWithTypicalSongLengths(t *testing.T) {
+	d := DurationFromString("03:01")
+	assert.Equal(t, 3.0, math.Floor(d.Minutes()))
+	assert.Equal(t, 181.0, d.Seconds())
+	d = DurationFromString("09:09")
+	assert.Equal(t, 9.0, math.Floor(d.Minutes()))
+	assert.Equal(t, 549.0, d.Seconds())
+}
+
+func TestDurationFromStringWithTypicalMovieLengths(t *testing.T) {
+	d := DurationFromString("02:15:03")
+	assert.Equal(t, 2.0, math.Floor(d.Hours()))
+	assert.Equal(t, 135.0, math.Floor(d.Minutes()))
+	assert.Equal(t, 8103.0, d.Seconds())
+}
